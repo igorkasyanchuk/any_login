89dad0a01e4f55234ee19daf928ceba0f334b0d3
Charles Maresh <cmaresh@gmail.com>
2017-10-12 23:08:34 -0500

    Allow override of the Devise sign in logic
    Override the default sign in behavior of Devise (and later other
    providers) by configuring a `AnyLogin.sign_in` proc to execute.

--- lib/any_login/providers/devise.rb
+++ lib/any_login/providers/devise.rb
@@ -4,14 +4,21 @@ module AnyLogin
 
       module Controller
 
+        DEFAULT_SIGN_IN = proc do |loginable|
+          reset_session
+          sign_in AnyLogin.klass.to_s.underscore.to_sym, loginable
+        end
+
         def self.any_login_current_user_method
           @@any_login_current_user_method ||= "current_#{AnyLogin.klass.to_s.underscore}".to_sym
         end
 
         def any_login_sign_in
-          reset_session
           @loginable = AnyLogin.klass.find(params[:selected_id].presence || params[:id])
-          sign_in AnyLogin.klass.to_s.underscore.to_sym, @loginable
+
+          sign_in = AnyLogin.sign_in || DEFAULT_SIGN_IN
+          instance_exec(@loginable, &sign_in)
+
           redirect_to main_app.send(AnyLogin.redirect_path_after_login)
         end
 
