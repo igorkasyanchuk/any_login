79f5c7363c45afc1a90add73df75381b2a44eee9
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-09-18 23:26:35 +0900

    Fix JavaScript

--- app/assets/javascripts/any_login/application.js
+++ app/assets/javascripts/any_login/application.js
@@ -1,25 +1,4 @@
-function submitOnChange() {
-  document.getElementById('any_login_form').submit();
-}
-
-function elementExists(element) {
-  return typeof (element) != 'undefined' && element != null;
-}
-
-function addChangeListenerTo(elementId) {
-  let element = document.getElementById(elementId);
-  if (elementExists(element)) {
-    element.addEventListener("change", submitOnChange);
-  }
-}
-
 function setupAnyLoginHandlers() {
-  addChangeListenerTo("back_to_previous_id");
-  addChangeListenerTo("selected_id");
-}
-
-['DOMContentLoaded', 'turbolinks:load', 'turbo:load'].forEach(function(e) {
-  // window.addEventListener(e, setupAnyLoginHandlers);
   const forms = document.querySelectorAll('.any_login_form');
   forms.forEach(form => {
     const selects = form.querySelectorAll('select');
@@ -29,4 +8,8 @@ function setupAnyLoginHandlers() {
       })
     })
   })
+}
+
+['DOMContentLoaded', 'turbolinks:load', 'turbo:load'].forEach(function(e) {
+  window.addEventListener(e, setupAnyLoginHandlers);
 });
