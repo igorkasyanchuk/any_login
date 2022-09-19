a432eb31eba23e6acde7e59db4a5bc3b4a240193
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-09-19 12:39:46 +0900

    Fix view to pass tests

--- test/devise/integration/navigation_test.rb
+++ test/devise/integration/navigation_test.rb
@@ -14,7 +14,7 @@ class NavigationTest < ActionDispatch::IntegrationTest
   test "it properly logs users in and allows access to the secret page" do
     visit "/"
 
-    find("#any_login_form_toggle_label").click
+    find(".any_login_form_toggle_label").click
     # There is an instability in the test suite where the option on the dropdown sometimes shows
     # up as `1` and sometimes shows up as `test@test.com`. This works around the issue.
     find('#selected_id option:last-of-type').select_option
