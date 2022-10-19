bc2976e7a64668b1285d4f9c24f29a37249e26cd
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-10-19 22:50:59 +0900

    Add Staff model

--- test/rails_apps/devise/db/schema.rb
+++ test/rails_apps/devise/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2015_09_09_210357) do
+ActiveRecord::Schema.define(version: 2022_10_19_135010) do
+
+  create_table "staffs", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
