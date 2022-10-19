d9f4ac2ae4eee28e1c76315ecc5bf32420626224
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-10-19 22:56:01 +0900

    Fix model

--- test/rails_apps/devise/app/models/staff.rb
+++ test/rails_apps/devise/app/models/staff.rb
@@ -1,4 +1,4 @@
-class Staff < ApplicationRecord
+class Staff < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
