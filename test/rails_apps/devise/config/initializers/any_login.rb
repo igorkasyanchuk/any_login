33e85c2ec2e4e86f3a77af3be5669be7de4a5793
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-10-19 22:54:13 +0900

    Fix klass_name to klass_names

--- test/rails_apps/devise/config/initializers/any_login.rb
+++ test/rails_apps/devise/config/initializers/any_login.rb
@@ -2,6 +2,6 @@ AnyLogin.setup do |config|
   config.enabled = true
   config.provider = :devise
   config.login_button_label = 'Sign in with Devise'
-  config.klass_name = 'User'
+  config.klass_names = ['User', 'Staff']
   config.collection_method = :all
 end
