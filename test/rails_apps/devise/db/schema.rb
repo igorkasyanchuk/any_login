3c84965e3f8e6416ee233e5ebd22757b61129ef6
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-10-19 23:00:41 +0900

    Add test staffs

--- test/rails_apps/devise/db/schema.rb
+++ test/rails_apps/devise/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_19_135121) do
+ActiveRecord::Schema.define(version: 2022_10_19_135937) do
 
   create_table "staffs", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_10_19_135121) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "role"
     t.index ["email"], name: "index_staffs_on_email", unique: true
     t.index ["reset_password_token"], name: "index_staffs_on_reset_password_token", unique: true
   end
